d13a763c3fd99964435af603a29b84266d28983f
mrafl <mrafliraihansyah@gmail.com>
2022-05-30 15:14:11 +0700

    add home page

---
+++ app/src/main/java/capstoneproject/mediscan/view/Home.kt
@@ -0,0 +1,12 @@
+package capstoneproject.mediscan.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import capstoneproject.mediscan.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
