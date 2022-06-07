237b20ea01329081243def426e9cfbcb8b765d55
mrafl <mrafliraihansyah@gmail.com>
2022-06-07 11:50:50 +0700

    detail activity not finish

--- app/src/main/java/capstoneproject/mediscan/view/DetailActivity.kt
+++ app/src/main/java/capstoneproject/mediscan/view/DetailActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
@@ -13,45 +12,26 @@ import capstoneproject.mediscan.R
 import capstoneproject.mediscan.data.MainViewModel
 import capstoneproject.mediscan.data.ViewModelFactory
 import capstoneproject.mediscan.data.local.UserPreferences
-import capstoneproject.mediscan.databinding.ActivityHomeBinding
+import capstoneproject.mediscan.databinding.ActivityDetailBinding
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "session")
 
 
-class HomeActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityHomeBinding
-
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_detail)
 
         val viewModel = ViewModelProvider(this,
             ViewModelFactory(UserPreferences.getInstance(dataStore))
         )[MainViewModel::class.java]
-        val isJustLogin = intent.getBooleanExtra(LOGIN_FLAG, false)
 
-        if (!isJustLogin) {
-            viewModel.getToken().observe(this) {
-                if (it.isEmpty()) {
-                    startActivity(Intent(this, LoginActivity::class.java))
-                    finish()
-                }
-            }
-        }
-
-        binding.buttonStart.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
-        }
-        binding.userLogo.setOnClickListener {
-            val intent = Intent(this@HomeActivity, DetailActivity::class.java)
+        binding.buttonLogout.setOnClickListener{
+            viewModel.deleteToken()
+            val intent = Intent(this@DetailActivity, LoginActivity::class.java)
             startActivity(intent)
         }
-
-    }
-
-    companion object {
-        const val LOGIN_FLAG = "LOGIN_FLAG"
     }
 
 }
