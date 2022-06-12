57d80714757185af54248023a12c3cb94e97f242
Muhammad Alif <muhammadalifputradafi@gmail.com>
2022-06-12 10:04:07 +0700

    Add redirect to map

--- app/src/main/java/capstoneproject/mediscan/view/CancerActivity.kt
+++ app/src/main/java/capstoneproject/mediscan/view/CancerActivity.kt
@@ -2,9 +2,10 @@ package capstoneproject.mediscan.view
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import capstoneproject.mediscan.R
 import capstoneproject.mediscan.databinding.ActivityCancerBinding
 import com.bumptech.glide.Glide
 
@@ -24,7 +25,10 @@ class CancerActivity : AppCompatActivity() {
             .into(binding.resultImage)
 
         binding.hospitalBtn.setOnClickListener{
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            val gmmIntentUri = Uri.parse(getString(R.string.URI_nearest_hospital))
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
         }
         binding.userLogo.setOnClickListener {
             val intent = Intent(this@CancerActivity, DetailActivity::class.java)
--- app/src/main/java/capstoneproject/mediscan/view/SickActivity.kt
+++ app/src/main/java/capstoneproject/mediscan/view/SickActivity.kt
@@ -2,9 +2,11 @@ package capstoneproject.mediscan.view
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import capstoneproject.mediscan.R
 import capstoneproject.mediscan.databinding.ActivitySickBinding
 import com.bumptech.glide.Glide
 
@@ -23,7 +25,10 @@ class SickActivity : AppCompatActivity() {
             .into(binding.resultImage)
 
         binding.hospitalBtn.setOnClickListener{
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            val gmmIntentUri = Uri.parse(getString(R.string.URI_nearest_hospital))
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
         }
         binding.userLogo.setOnClickListener {
             val intent = Intent(this@SickActivity, DetailActivity::class.java)
